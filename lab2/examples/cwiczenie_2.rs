efeb38ec31362f815d3eb45b953a8f07739624a0
JakubFr4czek <jakubfraczek.m8@gmail.com>
2024-03-31 14:27:10 +0200

    Finished lab2

--- lab2/examples/cwiczenie_2.rs
+++ lab2/examples/cwiczenie_2.rs
@@ -3,18 +3,25 @@
 // - ścieżkę żądania,
 // - parametry zapytania
 
-fn decomposeURL(input : &str){
+fn decomposeURL(input : &str) -> (String, String, String){
 
-    let domainStart = input.find("://") as u32;
-    let domainEnd = input.find("/") as u32;
+    let domain_start = input.find("://").unwrap();
+    let domain_end = domain_start + 3 + input[domain_start + 3..].find("/").unwrap();
 
-    println!("{}, {}", domainStart, domainEnd);
+    let path_start = domain_end;
+    let path_end = path_start + input[path_start..].find("?").unwrap();
 
+    // println!("{}, {}", domain_start, domain_end);
+    return (input[domain_start + 3..domain_end].to_string(),
+            input[path_start..path_end].to_string(),
+            input[path_end + 1..].to_string());
 }
 
 fn main(){
 
     let url = String::from("https://www.example.com/path/to/resource?param1=value1&param2=value2");
-    decomposeURL(&url);
+    let (_url, _path, _arguments) = decomposeURL(&url);
+
+    println!("url = {_url}, path = {_path}, arguments = {_arguments}");
 
 }
